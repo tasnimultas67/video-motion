7649df7523e6991f3c85985ad5c2e1fa5d2cc249
Tasnimul Haque <tasnimul.haque6@gmail.com>
2025-02-17 15:38:51 +0600

    Add search functionality and enhance header layout with icons

--- app/components/Header.js
+++ app/components/Header.js
@@ -5,6 +5,7 @@ import { Dialog, DialogPanel } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import Image from "next/image";
+import { Bell, Plus, Search } from "lucide-react";
 
 const navigation = [
   { name: "Product", href: "#" },
@@ -51,7 +52,7 @@ export default function Header() {
               <Bars3Icon aria-hidden="true" className="size-6" />
             </button>
           </div>
-          <div className="hidden lg:flex lg:gap-x-12">
+          {/* <div className="hidden lg:flex lg:gap-x-12">
             {navigation.map((item) => (
               <a
                 key={item.name}
@@ -61,11 +62,44 @@ export default function Header() {
                 {item.name}
               </a>
             ))}
+          </div> */}
+
+          {/* Search Button */}
+          <div className="group flex items-center">
+            <div className="flex h-9 md:ml-10 md:pl-5 border border-gray-400 rounded-l-3xl group-focus-within:border-black md:group-focus-within:ml-5 md:group-focus-within:pl-0 ">
+              <div className="w-10 justify-center items-center hidden group-focus-within:md:flex">
+                {/* <IoIosSearch className="text-black/[0.7] dark:text-white text-xl" /> */}
+                <Search className="size-4" />
+              </div>
+              <input
+                type="text"
+                // value={searchQuery}
+                placeholder="Search"
+                // onChange={(e) => setSearchQuery(e.target.value)}
+                // onKeyUp={searchQueryHandler}
+                className="w-44 px-5 bg-transparent outline-none text-black dark:text-white md:pl-0 md:group-focus-within:pl-0 md:w-64 lg:w-[500px]"
+              />
+            </div>
+            <button
+              // onClick={searchQueryHandler2}
+              className="w-[40px] md:w-[60px] h-8 md:h-9 flex items-center justify-center border border-l-0 border-gray-400 rounded-r-3xl bg-gray-100"
+            >
+              <Search className="size-5" />
+            </button>
           </div>
-          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <a href="#" className="text-sm/6 font-semibold text-gray-900">
-              Log in <span aria-hidden="true">&rarr;</span>
-            </a>
+
+          <div className="hidden lg:flex lg:flex-1 lg:justify-end items-center gap-5">
+            <button className="flex items-center justify-center gap-2 bg-gray-100 rounded-full p-2 px-3 text-sm">
+              <Plus className="size-5"></Plus> Create
+            </button>
+            <Bell className="size-6"></Bell>
+            <Image
+              src="https://tasnimul.vercel.app/_next/image?url=%2FTasnimul-Haque-3.jpg&w=1080&q=75"
+              width={30}
+              height={30}
+              alt="Account"
+              className="size-8 rounded-full"
+            ></Image>
           </div>
         </nav>
         <Dialog
