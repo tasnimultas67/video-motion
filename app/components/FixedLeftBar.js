d65409d335a33a7621cfbe6ebc55d12a3b791b77
Tasnimul Haque <tasnimul.haque6@gmail.com>
2025-04-15 15:54:35 +0600

    fix: add click handlers to buttons in FixedLeftBar for modal functionality

--- app/components/FixedLeftBar.js
+++ app/components/FixedLeftBar.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import { BoltIcon, HomeIcon, TvIcon } from "@heroicons/react/24/solid";
 import { ArrowDownToLine, UserCircle } from "lucide-react";
 import { useRouter } from "next/navigation";
+import WarningModal from "./WarningModal";
 
 const FixedLeftBar = () => {
   const [wModal, setWModal] = useState(false);
@@ -11,21 +12,37 @@ const FixedLeftBar = () => {
   return (
     <>
       <div className="fixed top-14 h-screen w-20 m-auto z-[1000]">
-        <button className="flex items-center justify-center flex-col text-[10px] rounded-lg m-auto text-center p-3 hover:bg-gray-200 w-16">
+        <button
+          onClick={() => setWModal(!wModal)}
+          className="flex items-center justify-center flex-col text-[10px] rounded-lg m-auto text-center p-3 hover:bg-gray-200 w-16"
+        >
           <HomeIcon className="size-5"></HomeIcon> Home
         </button>
-        <button className="flex items-center justify-center flex-col text-[10px] rounded-lg m-auto text-center p-3 hover:bg-gray-200 w-16">
+        <button
+          onClick={() => setWModal(!wModal)}
+          className="flex items-center justify-center flex-col text-[10px] rounded-lg m-auto text-center p-3 hover:bg-gray-200 w-16"
+        >
           <BoltIcon className="size-5"></BoltIcon> Shorts
         </button>
-        <button className="flex items-center justify-center flex-col text-[10px] rounded-lg m-auto text-center p-3 hover:bg-gray-200 w-16">
+        <button
+          onClick={() => setWModal(!wModal)}
+          className="flex items-center justify-center flex-col text-[10px] rounded-lg m-auto text-center p-3 hover:bg-gray-200 w-16"
+        >
           <TvIcon className="size-5"></TvIcon> Subscription
         </button>
-        <button className="flex items-center justify-center flex-col text-[10px] rounded-lg m-auto text-center p-3 hover:bg-gray-200 w-16">
+        <button
+          onClick={() => setWModal(!wModal)}
+          className="flex items-center justify-center flex-col text-[10px] rounded-lg m-auto text-center p-3 hover:bg-gray-200 w-16"
+        >
           <UserCircle className="size-5"></UserCircle> You
         </button>
-        <button className="flex items-center justify-center flex-col text-[10px] rounded-lg m-auto text-center p-3 hover:bg-gray-200 w-16">
+        <button
+          onClick={() => setWModal(!wModal)}
+          className="flex items-center justify-center flex-col text-[10px] rounded-lg m-auto text-center p-3 hover:bg-gray-200 w-16"
+        >
           <ArrowDownToLine className="size-5"></ArrowDownToLine> Downlaods
         </button>
+        <WarningModal wModal={wModal} setWModal={setWModal} />
       </div>
     </>
   );
